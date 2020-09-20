bd58a4ccbd4f8de59ab5239bc211edfb7794dcc9
Stv <dan.steve2910@gmail.com>
2020-09-20 12:27:48 +0700

    upload

---
+++ js/extra.js
@@ -0,0 +1,9 @@
+$("#condition button").on("click", function(){
+	$("#condition button").removeClass("active");
+	$(this).addClass("active");
+})
+
+$("#pricebuttons button").on("click", function(){
+	$("#pricebuttons button").removeClass("active");
+	$(this).addClass("active");
+})
